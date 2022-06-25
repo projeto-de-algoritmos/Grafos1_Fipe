3e100980294a62a1ba21641b896f745b5fa027b9
Gustave Persijn <gpersijn@outlook.com>
2022-06-25 17:23:11 -0300

    adding elements in select

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const bodyParser = require('body-parser');
+const data = require('./data/data.js');
+const Graph = require('./graph_functions/graph.js');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-const Graph = require('./graph_functions/graph');
-const graph = new Graph();
-
 app.use(express.static('static'));
-app.use("/static", express.static(__dirname + "static/index.js"));
+app.use('/css', express.static(__dirname + 'static/css'))
+app.use('/img', express.static(__dirname + 'static/img'))
+app.use('/js', express.static(__dirname + 'static/img'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.get('', function (req, res) {
--- static/js/itensSelect.js
+++ static/js/itensSelect.js
@@ -1,153 +1,27 @@
-
-
+const { Console } = require('console');
+const data = require('../../data/carros.json');
 
 //gerar as op��es pro select
-function geraOptions(vet, id) {
+function geraOptions() {
+    
+    let optionSelect = document.getElementById("select-modelo")
 
-    let optionSelect = document.getElementById(id)
-    for (let i = 0; i < vet.length; i++) {
+    for (var item in data){
         let option = document.createElement("option")
-        option.value = vet[i]
-        option.text = vet[i]
-        optionSelect.appendChild(option)
+        let modelo = data[item].modelo;
 
+        console.log(modelo)
+        
+        option.value = modelo;
+        
+        option.text = modelo;
+        optionSelect.appendChild(option)
     }
 
 }
 
-function listarArrays() {
+geraOptions()
 
-    var idDinastia = "select-dinastia";
-    var idNome = "select-nome";
-    var idCidadeNatal = "select-cidade";
-    var idCausaMorte = "select-causa";
-    var dinastia = ['Dinastia',
-        'Constantinian',
-        'Gordian',
-        'Julio-Claudian',
-        'Flavian',
-        'Severan',
-        'Nerva-Antonine',
-        'Valentinian',
-        'Theodosian']
-    var nome = ['Nome',
-        'Galerius',
-        'Lucinius',
-        'Philip I',
-        'Trebonianus Gallus',
-        'Balbinus',
-        'Aurelian',
-        'Numerian',
-        'Carinus',
-        'Tiberius',
-        'Galba',
-        'Didius Julianus',
-        'Geta',
-        'Carus',
-        'Julian',
-        'Constans',
-        'Augustus',
-        'Trajan Decius',
-        'Tacitus',
-        'Probus',
-        'Elagabalus',
-        'Maximinus I',
-        'Gallienus',
-        'Gordian I',
-        'Marcus Aurelius',
-        'Domitian',
-        'Vitellius',
-        'Constantius I',
-        'Hostilian',
-        'Caracalla',
-        'Claudius Gothicus',
-        'Constantine II',
-        'Jovian',
-        'Gordian II',
-        'Pupienus',
-        'Constantine the Great',
-        'Severus Alexander',
-        'Diocletian',
-        'Valens',
-        'Valentinian II',
-        'Valentinian I',
-        'Theodosius I',
-        'Vespasian',
-        'Commodus',
-        'Pertinax',
-        'Hadrian',
-        'Trajan',
-        'Otho',
-        'Macrinus',
-        'Quintillus',
-        'Florian',
-        'Nero',
-        'Vetranio',
-        'Severus II',
-        'Claudius',
-        'Lucius Verus',
-        'Septimus Severus',
-        'Antonius Pius',
-        'Nerva',
-        'Caligula',
-        'Titus',
-        'Gratian',
-        'Maximian',
-        'Valerian',
-        'Aemilian',
-        'Maximinus II',
-        'Maxentius',
-        'Gordian III',
-        'Constantius II'];
-    var cidadeNatal = ['Cidade natal',
-        'Romuliana',
-        'Felix Romuliana',
-        'Shahba',
-        '',
-        'Sirmium',
-        'Rome',
-        'Terracina',
-        'Milan',
-        'Narbo',
-        'Constantinople',
-        'Budalia',
-        'Interamna Nahars',
-        'Emesa',
-        'Dardania',
-        'Lugdunum',
-        'Arelate',
-        'Singidunum',
-        'Naissus',
-        'Arca Caesarea',
-        'Salona',
-        'Cibalae',
-        'Cauca',
-        'Falacrine',
-        'Lanuvium',
-        'Alba',
-        'Italica',
-        'Terentinum',
-        'Iol Caesarea',
-        'Antitum',
-        'Leptis Magna',
-        'Narni',
-        'Sirmium'];
-    var causaMorte = ['Causa da morte',
-        'Natural Causes',
-        'Execution',
-        'Assassination',
-        'Unknown',
-        'Died in Battle',
-        'Suicide',
-        'Captivity'];
-    geraOptions(dinastia, idDinastia);
-    geraOptions(nome, idNome);
-    geraOptions(cidadeNatal, idCidadeNatal);
-    geraOptions(causaMorte, idCausaMorte);
-}
-
-
-listarArrays()
 
 
 
